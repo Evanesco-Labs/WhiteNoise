da5da69de5d04a0a003864b6e6cad5fc535b20eb
CaleHH <caleihhmy@gmail.com>
2021-04-08 10:31:07 +0800

    add relay message type

--- secure/crypto.go
+++ secure/crypto.go
@@ -6,14 +6,14 @@ import (
 
 func (s *SecureSession) encrypt(out, plaintext []byte) ([]byte, error) {
 	if s.enc == nil {
-		return nil, errors.New("cannot encrypt, handshake incomplete")
+		return nil, errors.New("cannot encrypt, no handshake or handshake failed")
 	}
 	return s.enc.Encrypt(out, nil, plaintext), nil
 }
 
 func (s *SecureSession) decrypt(out, ciphertext []byte) ([]byte, error) {
 	if s.dec == nil {
-		return nil, errors.New("cannot decrypt, handshake incomplete")
+		return nil, errors.New("cannot decrypt, no handshake or handshake failed")
 	}
 	return s.dec.Decrypt(out, nil, ciphertext)
 }
