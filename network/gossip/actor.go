993fae3f1ecea3d32d4cee49432555c29813acaa
CaleHH <caleihhmy@gmail.com>
2021-05-04 16:42:22 +0800

    add DHT service

---
+++ network/gossip/actor.go
@@ -0,0 +1,19 @@
+package gossip
+
+import (
+	"github.com/AsynkronIT/protoactor-go/actor"
+	"whitenoise/internal/actorMsg"
+)
+
+func (service *DHTService) Receive(ctx actor.Context) {
+	switch msg := ctx.Message().(type) {
+	case actorMsg.ReqGossipJoint:
+		err := service.GossipJoint(msg.DesHash, msg.NegCypher)
+		ctx.Respond(actorMsg.ResError{Err: err})
+	case actorMsg.ReqDHTPeers:
+		peerInfos := service.GetDHTPeers(msg.Max)
+		ctx.Respond(actorMsg.ResDHTPeers{PeerInfos: peerInfos})
+	default:
+		//log.Debugf("Gossip actor cannot handle this request %v", msg)
+	}
+}
